dae745e227c46f5ea58571d3fcccebc33e535241
shaukathpatan@gmail.com <shaukathpatan@gmail.com>
2018-03-13 23:21:32 +0530

    Added Grid to new component with service and model classes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { GameService } from './game/game.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MatGridListModule
   ],
-  providers: [],
+  providers: [GameService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent implements OnInit {
+
+    tiles: Array<any> = [];
+  constructor() { }
+
+  ngOnInit() {
+
+    this.tiles = [
+      {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+      {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+      {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+      {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+    ];
+  }
+
+}
